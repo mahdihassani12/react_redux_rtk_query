9b3f4e1b7fbf206b019ce3798092c4ddcf8116ae
Mahdi Hassai <hassanim430@gmail.com>
2023-08-27 18:51:28 +0430

    adding time ago and reaction buttons

--- lesson-2/src/features/posts/postsSlice.js
+++ lesson-2/src/features/posts/postsSlice.js
@@ -6,11 +6,29 @@ const initialState = [
     id: "1",
     title: "Learning Redux Toolkit",
     content: "I've heard good things.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    userId: "0",
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    userId: "1",
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -28,14 +46,29 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
-            userId
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer;
