16d63ff55256317cb64d611bf583bc909b6ff550
Mahdi Hassai <hassanim430@gmail.com>
2023-08-27 18:27:56 +0430

    add posts forms

--- lesson-2/src/features/posts/postsSlice.js
+++ lesson-2/src/features/posts/postsSlice.js
@@ -1,40 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { sub } from 'date-fns';
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: "1",
     title: "Learning Redux Toolkit",
     content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
   },
   {
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
   },
 ];
 
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    postAdded: (state, action) => {
+      state.push(action.payload);
+    },
+  },
 });
 
 export const selectAllPosts = (state) => state.posts;
+export const { postAdded } = postsSlice.actions;
 export default postsSlice.reducer;
